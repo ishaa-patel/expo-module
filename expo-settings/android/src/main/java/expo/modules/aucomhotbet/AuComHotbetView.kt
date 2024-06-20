b20a26d31d022757bb45a0a2f21f73c71a5fad47
Isha <ishapatel2401@gmail.com>
2024-06-20 12:53:43 +0530

    update & remove of file function
    made some changes according to native module documents to run the example project

--- expo-settings/android/src/main/java/expo/modules/aucomhotbet/AuComHotbetView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.aucomhotbet
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class AuComHotbetView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
